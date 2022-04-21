281072993b63b478e40399ea6a4442c48a71726c
DiederikDS <diederik.desoete96@gmail.com>
2022-04-21 16:01:00 +0200

    Created an application to store contact information

---
+++ ContactsApplication/DesktopContactsApp/DesktopContactsApp/Classes/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace DesktopContactsApp.Classes
+{
+    public class Contact
+    {
+        //Properties that map to columns in the sql database
+
+        //SQL attributes can be defined between "[]" (due to NuGet package sqlite-net-pcl)
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        //We need to override the ToString methode because when reading the database, the listview does not understand how to display complex object and defaults to the ToString methode.
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
+
+    }
+}
---
+++ ContactsApplication/DesktopContactsApp/DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Contact> contacts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contacts = new List<Contact>();
+
+            ReadDataBase();
+        }
+
+        //Makes a NewContactWindow appear when the button "New Contact" is clicked.
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            ReadDataBase();
+        }
+
+        void ReadDataBase()
+        {
+
+            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                //We need to have at least 1 table to read from or the application will crash (ReadDatabase is called in the constructor of the MainWindow)
+                connection.CreateTable<Contact>();
+                //we create a list of Contact objects read from the contact table.
+                contacts = (connection.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+            }
+
+            if (contacts != null)
+            {
+
+                contactListView.ItemsSource = contacts;
+
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            contactListView.ItemsSource = filteredList;
+
+        }
+
+        private void contactListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactListView.SelectedItem;
+
+            if (selectedContact != null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+            }
+
+            ReadDataBase();
+
+        }
+    }
+}
