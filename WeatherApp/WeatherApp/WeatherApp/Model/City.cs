e5e7bad7eb0f5ea9fb1480f5b05a1356ddf513de
Diederik De Soete <71342558+DiederikDS@users.noreply.github.com>
2022-04-29 00:53:28 +0200

    Add files via upload

---
+++ WeatherApp/WeatherApp/WeatherApp/Model/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Model
+{
+
+    //This class was made through a Json file (api to get information on a city)
+    public class Area
+    {
+        public int ID { get; set; }
+
+        public string LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Area { get; set; }
+    }
+}
